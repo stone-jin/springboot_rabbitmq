d6c6d91354c1624ac35836da8a5de58419c8ce8b
金炳 <hzjinbing@corp.netease.com>
2017-12-07 23:22:24 +0800

    change: 项目初始化

---
+++ src/main/java/test_rabbitmq/TestRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package test_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRabbitmqApplication.class, args);
+	}
+}
